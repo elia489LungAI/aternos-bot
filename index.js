2a2fcbaf81de54045616fc8123df776f8bcf2b46
elia489LungAI <mohammed.sooo9hiy@gmail.com>
2026-08-16 22:08:36 +0300

    index.js

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ const bot = mineflayer.createBot({
 bot.on('spawn', () => {
   console.log("Bot joined and ready!");
   
-  // تكرار القفز والحركة كل 6 ثوانٍ (6000 ميللي ثانية)
+  // Jump and look around every 6 seconds
   setInterval(() => {
     bot.setControlState('jump', true);
-    setTimeout(() => bot.setControlState('jump', false), 500); // يفعل القفزة لنصف ثانية ثم يوقفها
-    bot.look(bot.entity.yaw + 1.5, 0); // يفر راسه شوية حتى يضمن عدم اعتباره AFK
+    setTimeout(() => bot.setControlState('jump', false), 500);
+    bot.look(bot.entity.yaw + 1.5, 0);
   }, 6000);
 });
 
 bot.on('end', () => {
   console.log("Bot disconnected, reconnecting in 5 seconds...");
-  setTimeout(() => process.0_exit(0), 5000); // ملاحظة: تأكد تكون process.exit(0) مثل كودك القديم
+  setTimeout(() => process.exit(0), 5000);
 });
 
 bot.on('error', (err) => console.log(err));
