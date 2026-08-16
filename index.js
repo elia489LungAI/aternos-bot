774b18c560ec71e1514f295f4724a9d46dc726e3
elia489LungAI <mohammed.sooo9hiy@gmail.com>
2026-08-16 22:29:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,31 @@ const mineflayer = require('mineflayer');
 const bot = mineflayer.createBot({
   host: 'LungCraft.aternos.me',
   port: 57104,
-  username: 'AI.Bot'
+  username: 'AternosBot'
 });
 
 bot.on('spawn', () => {
   console.log("Bot joined and ready!");
   
-  // Jump and look around every 0.3 seconds
+  // Move forward and backward every 1 second
   setInterval(() => {
+    bot.setControlState('forward', true);
+    setTimeout(() => {
+      bot.setControlState('forward', false);
+      bot.setControlState('back', true);
+      setTimeout(() => bot.setControlState('back', false), 300);
+    }, 300);
+    
+    // Jump and look around to prevent AFK
     bot.setControlState('jump', true);
-    setTimeout(() => bot.setControlState('jump', false), 500);
+    setTimeout(() => bot.setControlState('jump', false), 200);
     bot.look(bot.entity.yaw + 1.5, 0);
-  }, 0300);
+  }, 1000);
 });
 
 bot.on('end', () => {
-  console.log("Bot disconnected, reconnecting in 0.3 seconds...");
-  setTimeout(() => process.exit(0), 0300);
+  console.log("Bot disconnected, reconnecting in 5 seconds...");
+  setTimeout(() => process.exit(0), 5000);
 });
 
 bot.on('error', (err) => console.log(err));
